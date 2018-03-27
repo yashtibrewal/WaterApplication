84a71cf09dff90e1c31031151377c279b50f09ab
Yash Tibrewal <yashkush.tibrewal@gmail.com>
2018-03-27 18:03:56 +0530

    created the strarting page basic layout

--- app/src/main/java/com/example/dell_7560/waterapplication/MainActivity.java
+++ app/src/main/java/com/example/dell_7560/waterapplication/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.dell_7560.waterapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Spinner spinner=findViewById(R.id.id_spinner1);
+
+        ArrayAdapter<TextView> adapter =ArrayAdapter.createFromResource(this,R.array.);
+        //setting the new text view to the array adapter
+
+        spinner.setAdapter(adapter);
     }
 }
